f468beaa45a3f1b7e6d8e12f1c65b4658b01ddd8
Guilherme Sposito <guilhermesposito14@gmail.com>
2025-03-06 12:12:57 -0300

    Adicionando Mudanças de lançamento balcão

--- obj/Debug/net8.0-android/AppGarcomSys.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/AppGarcomSys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppGarcomSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a7b6555349bd653019aacede81d5494dc92093f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0f142f1e4ce9164db37017d4f40f32399d8c6bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppGarcomSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppGarcomSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
